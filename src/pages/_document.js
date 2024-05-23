d5cd7038f9480a7789174af75caae6f5a4794608
ilechukwu joshua <ilechuks73@gmail.com>
2024-05-23 18:55:58 +0100

    UI UPDATE

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className={"dark:bg-[#000000] dark:text-white"}>
+    <Html lang="en" className={""}>
       <Head>
         {/*<link*/}
         {/*  rel="shortcut icon"*/}
