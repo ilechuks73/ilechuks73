7b88164c9607c98a78571893fecfd7bf52bf0ce7
ilechukwu joshua <ilechuks73@gmail.com>
2024-05-23 01:10:23 +0100

    bug fix

--- src/components/root/Contact.js
+++ src/components/root/Contact.js
@@ -2,7 +2,7 @@ import { Button as MuiButton, TextField as MuiTextField } from "@mui/material";
 export default function Contact() {
   return (
     <div className={"font-roboto max-w-[1500px] mx-10 mt-20 xl:mx-auto"}>
-      <div className={"mt-10 flex gap-2 w-full"}>
+      <div className={"mt-10 flex flex-wrap sm:flex-nowrap gap-2 w-full"}>
         <div className={"w-full md:w-[50%] xl:w-[70%]"}>
           <h1 className={"text-[20px] font-medium"}>{"Let's Work Together"}</h1>
           <p className={"text-[15px] text-gray-600 mt-2"}>
