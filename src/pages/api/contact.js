74eadc3ce2a22156f41da5701debbb9e06233a2d
ilechukwu joshua <ilechuks73@gmail.com>
2024-05-24 23:40:39 +0100

    ADDING RECAPTCHA

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,4 +1,25 @@
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  const axios = require("axios");
+
+  const response2 = await axios.post(
+    "https://www.google.com/recaptcha/api/siteverify",
+    null,
+    {
+      params: {
+        secret: process.env.NEXT_PRIVATE_RECAPTCHA_SECRET_KEY,
+        response: request.body.token,
+      },
+    },
+  );
+
+  if (response2.data.success === false) {
+    return response.status(200).json({
+      message: "Failed",
+      error: true,
+      data: null,
+    });
+  }
+
   response.status(200).json({
     message: "Success",
     error: false,
