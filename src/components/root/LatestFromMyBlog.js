72f05629a38ec6193799422c4ef8e2efc4952fc9
ilechukwu joshua <ilechuks73@gmail.com>
2024-06-05 18:47:59 +0100

    UI UPDATE

--- src/components/root/LatestFromMyBlog.js
+++ src/components/root/LatestFromMyBlog.js
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import { Button as MuiButton } from "@mui/material";
+ 
 import { useState, useEffect } from "react";
 import services from "@/services";
 import { enqueueSnackbar } from "notistack";
-import moment from "moment";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { IoIosArrowRoundForward } from "react-icons/io";
+
+import { useRouter } from "next/router"; 
+
 import PostCardSkeleton from "@/components/PostCardSkeleton";
+import PostCard from "@/components/PostCard";
 
 export default function LatestFromMyBlog() {
   const router = useRouter();
@@ -43,7 +42,7 @@ export default function LatestFromMyBlog() {
       </div>
       <div
         className={
-          "mt-4 w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2"
+          "mt-4 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2"
         }
       >
         {isLoading[0] ? (
@@ -56,54 +55,7 @@ export default function LatestFromMyBlog() {
           <>
             {posts.map((item, index) => {
               return (
-                <div
-                  key={index}
-                  className={
-                    "border overflow-hidden border-gray-300 rounded-md"
-                  }
-                >
-                  <div
-                    className={
-                      "mb-5 h-[200px] overflow-hidden flex border border-gray-300 flex-col justify-center"
-                    }
-                  >
-                    <a
-                      href={`https://blog.ilechuks73.com/posts/${item._id}`}
-                      target="_blank"
-                    >
-                      <img
-                        src={item.image}
-                        alt={"ilechuks73"}
-                        className="object-fill"
-                      />
-                    </a>
-                  </div>
-                  <div className={"px-2 pb-5"}>
-                    <a
-                      href={`https://blog.ilechuks73.com/posts/${item._id}`}
-                      target="_blank"
-                    >
-                      <p className={"text-md truncate hover:underline"}>
-                        {item.title}
-                      </p>
-                    </a>
-                    <p className={"text-gray-500 text-sm"}>
-                      {moment(item.createdAt).format("LL")}
-                    </p>
-                    <a
-                      href={`https://blog.ilechuks73.com/posts/${item._id}`}
-                      target="_blank"
-                    >
-                      <MuiButton
-                        className={"!px-0 !text-black !hover:text-underline"}
-                        disableRipple={true}
-                        endIcon={<IoIosArrowRoundForward />}
-                      >
-                        Read
-                      </MuiButton>
-                    </a>
-                  </div>
-                </div>
+                <PostCard post={item} key={index}/>
               );
             })}
           </>
